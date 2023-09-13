3f02710d3f8c8ddd379eb78e8cd65c1f1669d42d
ibanferro <iban.ferro@gmail.com>
2023-09-13 23:09:11 +0200

    Initial page headers created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { SeriesComponent } from './components/series/series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', component: HomeComponent },
+  {path: 'series', component: SeriesComponent},
+  {path: 'movies', component: MoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SeriesComponent } from './components/series/series.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SeriesComponent,
-    MoviesComponent
+    MoviesComponent,
+    FooterComponent,
+    SubHeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
