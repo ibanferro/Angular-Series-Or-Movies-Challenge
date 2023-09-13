812e17f32c17f43f588da63f5cb5c25aab27efec
ibanferro <iban.ferro@gmail.com>
2023-09-13 22:40:12 +0200

    All main components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SeriesComponent } from './components/series/series.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SeriesComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
